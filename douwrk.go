501f981b4e91c379962daab5321299b35a521023
syedmuhammadhusain <55493276+syedmuhammadhusain@users.noreply.github.com>
2019-09-22 23:09:15 +0500

    Delete douwrk.go

--- douwrk.go
+++
@@ -1,9 +0,0 @@
-//Package greetings shows the greetings
-package greetings
-//GreetingsString is a global variable
-var GreetingsString = "Hello World"
-//PrintGreetings is a global function
-func PrintGreetings(name string) string {
-return GreetingsString + "-" + name
-
-}
