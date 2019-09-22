e75456a33060e131d823d032213b0b49a688ccbf
syedmuhammadhusain <55493276+syedmuhammadhusain@users.noreply.github.com>
2019-09-22 23:08:36 +0500

    Update assignment01IBC.go

--- assignment01IBC.go
+++ assignment01IBC.go
@@ -1,1 +1,46 @@
+import main
+import assignment01IBC
 //16i-0340
+
+type Block struct {
+	transaction string
+	prevPointer *Block
+	next *Block
+}
+
+func InsertBlock(transaction string, chainHead *Block) *Block {
+	if chainHead== nil{
+		return chainHead
+	}
+	for b := chainHead; b!=nil; b=b.next {
+		if b.next == nil{
+			b.next = &Block{transaction, b, nil}
+			return chainHead
+		}
+	}
+	return chainHead
+}
+func ListBlocks(chainHead *Block) {
+	for b := ListBlocks; b!=nil;b=b.next{
+		fmt.Println(b)
+	}
+}
+func ChangeBlock(oldTrans string, newTrans string, chainHead *Block) {
+	if chainHead== nil{
+		return
+	}
+	for b := chainHead; b!=nil; b=b.next {
+		if b.transaction == oldTrans{
+			b.transaction = newTrans
+		}
+		if b.next == nil{
+			if b.transaction == oldTrans {
+				b.transaction = newTrans
+			}
+			return
+		}
+	}
+}
+func VerifyChain(chainHead *Block) {
+
+}
